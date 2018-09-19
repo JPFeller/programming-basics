63a5a487ac51459272d933cb1369ef7d0517a25c
JPFeller <patu.feller@gmail.com>
2018-09-19 10:01:05 +0300

    update

--- IF training/IF training/Program.cs
+++ IF training/IF training/Program.cs
@@ -8,9 +8,18 @@ namespace IF_training
         static void Main(string[] args)
         {
          
-            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla.");
-            Console.WriteLine("Syötä luku")
-            
+         Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla.");
+         Console.Write("Syötä numero: ");
+         string userInput = Console.ReadLine();
+         int number = int.Parse(userInput)
+
+          if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla!");
+          else if (number < 0)
+                Console.WriteLine($"Numero{number} on negatiivinen!");
+          else
+                Console.WriteLine($"Numero {number} on positiivinen!");
+
         }
     }
 }
