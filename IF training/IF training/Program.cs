9977c006ddbebdade4973ece66761a7653ce22d3
JPFeller <patu.feller@gmail.com>
2018-09-19 09:08:17 +0300

    Update

---
+++ IF training/IF training/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IF_training
+{
+    // This one line comment
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla.");
+            Console.WriteLine("Syötä luku")
+            
+        }
+    }
+}
