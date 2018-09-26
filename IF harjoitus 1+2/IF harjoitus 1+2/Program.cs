ac876ba87e7e13ef81ec5a923d18fa916a5ee3aa
JPFeller <patu.feller@gmail.com>
2018-09-26 10:46:26 +0300

    if task 3

---
+++ IF harjoitus 1+2/IF harjoitus 1+2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IF_harjoitus_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko luku parillinen, pariton, negatiivinen, positiivinen tai nolla");
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            int.TryParse(userInput, out number);
+            bool isNumber = int.TryParse(userInput, out number);
+            if (isNumber)
+                if (number < 0)
+                    Console.WriteLine($"Numero {number} on negatiivinen");
+                    else if (number == 0)
+                        Console.WriteLine($"Numero {number} on nolla");
+                    else
+                        Console.WriteLine($"Numero {number} on positiivinen");
+            if (isNumber)
+                if (number % 2 == 0 && number != 0)
+                    Console.WriteLine($"Numero {number} on parillinen");
+                    else if (number == 0)
+                        Console.WriteLine($"Numero {number} on jakautumaton");
+                    else
+                        Console.WriteLine($"Numero {number} on pariton");
+            else
+                Console.WriteLine($"Väärä syöte");
+        }
+    }
+}
