6feebab80607e85b91b44e65e49a80e1072d1529
JPFeller <patu.feller@gmail.com>
2018-10-12 10:37:41 +0300

    new program done

---
+++ LoopTask3.3/LoopTask3.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopTask3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee ensimmäisten parittomien ja parillisten positiivisten lukujen summan");
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i;
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}");
+            Console.WriteLine($"Parittomien summa on {oddSum}");
+
+        }
+    }
+}
