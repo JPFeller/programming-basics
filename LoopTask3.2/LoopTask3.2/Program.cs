e41c92f393f9251a3e798ac1d05e4f6bd54f8784
JPFeller <patu.feller@gmail.com>
2018-10-12 10:37:03 +0300

    new program done

---
+++ LoopTask3.2/LoopTask3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopTask3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen");
+            int number = 0;
+            int i = 1;
+            int sum = 1;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("Virheellinen syötä");
+                }
+            } while (number <= 0);
+
+            while (i <= number)
+            {
+                sum = sum + i;
+                i = i + 1;
+            }
+            Console.WriteLine($"Luvun {number} summa = {sum}");
+
+        }
+    }
+}
