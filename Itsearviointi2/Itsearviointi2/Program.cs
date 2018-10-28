e1c051fab0423b9ce9d6565aedb79baddc3b54cf
JPFeller <patu.feller@gmail.com>
2018-10-28 19:53:14 +0200

    new task done

---
+++ Itsearviointi2/Itsearviointi2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Itsearviointi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjältä sanan tai lauseen ja tulostaa sen niin monta kertaa näytölle, kun sanassa on kirjaimia");
+            Console.WriteLine("Syötä sana tai lause: ");
+            string userInput = Console.ReadLine();
+
+            int len = userInput.Length;
+            int j = 0;
+
+            Console.WriteLine($"Merkkien lukumäärä: {len}");
+            for (int i = 0; i < len; i++)
+            {
+                j = j + 1;
+                Console.WriteLine($"{j}. {userInput}");
+            }
+        }
+    }
+}
