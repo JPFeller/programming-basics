6995cc1a7217382fe61eb3cabca5154c6d2871e2
JPFeller <patu.feller@gmail.com>
2018-10-28 19:53:38 +0200

    new task done

---
+++ Itsearviointi3/Itsearviointi3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Itsearviointi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Ohjelma kysyy käyttäjältä lukuja, kunnes hän syöttää luvun -1. Ohjelma tulostaa näytölle syötettyjen lukujen summan");
+                int number = 0;
+                int sum = 0;
+                do
+                {
+                    Console.WriteLine("Syötä kokonaisluku: ");
+                    number = int.Parse(Console.ReadLine());
+                    sum = sum + number;
+                }
+                while (number != -1);
+                Console.WriteLine($"{sum + 1}");
+
+          
+        }
+    }
+}
