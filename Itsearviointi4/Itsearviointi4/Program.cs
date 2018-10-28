1544218316c584f38fff37e8d1c2d645ea8c7947
JPFeller <patu.feller@gmail.com>
2018-10-28 20:00:53 +0200

    new task done

---
+++ Itsearviointi4/Itsearviointi4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Itsearviointi4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma pyytää kaksi lukua ja tulostaa perusaritmeettiset laskutoimitukset");
+            double number1 = 0;
+            double number2 = 0;
+            int k = 0;
+
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Syötä toinen luku: ");
+            number2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{k}. {number1}+{number2} = {number1 + number2}");
+            k = k + 1;
+            Console.WriteLine($"{k}. {number1}-{number2} = {number1 - number2}");
+            k = k + 1;
+            Console.WriteLine($"{k}. {number1}*{number2} = {number1 * number2}");
+            k = k + 1;
+            Console.WriteLine($"{k}. {number1}/{number2} = {number1 / number2}");
+            k = k + 1;
+        }
+    }
+}
