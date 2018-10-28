6e0d1487f75a337b17291b150e5a87ce12171640
JPFeller <patu.feller@gmail.com>
2018-10-28 19:37:20 +0200

    new task done

---
+++ FunctionTask 5.3/FunctionTask 5.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FunctionTask_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Käyttäjältä pyydetän luku, joka on väliltä 1-10 ja ohjelma tarkistaa, onko luku sallittu");
+                int number = numberFromRange(1, 10);
+                Console.WriteLine($"Luku {number} on sallittu");
+            }
+            int numberFromRange(int lowerBound, int upperBound)
+            {
+                int allowedNumber;
+                while (true)
+                {
+                    Console.Write("Syötä luku: ");
+                    bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                    if (userInput >= lowerBound && userInput <= upperBound)
+                    {
+                        allowedNumber = userInput;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Luku ei ole sallittu");
+                    }
+                }
+                return allowedNumber;
+            }
+
+        }
+    }
+}
