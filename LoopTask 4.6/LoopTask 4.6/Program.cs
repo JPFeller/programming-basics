a7777877b6263ff6927f9d6cf0703cda77b800e2
JPFeller <patu.feller@gmail.com>
2018-10-28 19:09:53 +0200

    new task done

---
+++ LoopTask 4.6/LoopTask 4.6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopTask_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numberSix = 0;
+
+            Console.WriteLine("Ohjelma heitää noppaa 1000 kertaa ja tulostaa kuutosen esiintymiskertojen lukumäärän");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                int x = rnd.Next(1, 7);
+
+                if (x == 6)
+                {
+                    numberSix++;
+                }
+                Console.WriteLine("{0}. {1}", i, x);
+            }
+            Console.WriteLine("Kutonen arvottiin {0} kertaa.", numberSix);
+        }
+    }
+}
