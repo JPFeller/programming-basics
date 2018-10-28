76232a4d1dfc5fc218275595473771c4be5aa84b
JPFeller <patu.feller@gmail.com>
2018-10-28 19:12:13 +0200

    minor changes

--- LoopTask 4.1/LoopTask 4.1/Program.cs
+++ LoopTask 4.1/LoopTask 4.1/Program.cs
@@ -6,27 +6,12 @@ namespace LoopTask_4._1
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int rndNumber;
-
             Console.WriteLine("Ohjelma tulostaa 10 arvottua kokonaislukua ja niiden neliöjuuret.");
             Console.WriteLine("#\tRND\tSQRT");
-            #region for example
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    rndNumber = rnd.Next(2, 10);
-            //    Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
-            //}
-            #endregion
+            Random rnd = new Random();
+            int rndNumber;
             int i = 0;
-            #region while example
-            //while (i<10)
-            //{
-            //    rndNumber = rnd.Next(2, 10);
-            //    Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
-            //    i = i + 1; //i++
-            //}
-            #endregion
+
             do
             {
                 rndNumber = rnd.Next(2, 10);
